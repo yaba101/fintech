2941c3c58182565f390d5079935530494a798b6b
Yeabsira Mekuria <yabumek46@gmail.com>
2023-11-21 23:49:15 +0300

    fix some issues

--- src/components/AssetDebtStats.tsx
+++ src/components/AssetDebtStats.tsx
@@ -47,8 +47,8 @@ const AssetDebtStats = async () => {
           <TrendingDown className="text-gray-100 dark:text-gray-100 xs:h-4 xs:w-4 md:h-8 md:w-8 lg:h-5 lg:w-5 xl:h-8 xl:w-8" />
         }
         iconBgColor="bg-[#27674a]"
-        textBgColor="bg-green-400"
-        textColor="text-emerald-400"
+        textBgColor="bg-green-300"
+        textColor="text-green-700 dark:text-green-200"
         amount={asset}
         signIcon={
           <Add className="text-green-600 dark:text-green-600 xs:h-4 xs:w-2 md:h-5 md:w-3" />
@@ -60,8 +60,8 @@ const AssetDebtStats = async () => {
         }
         title="Total Debt"
         iconBgColor="bg-red-600"
-        textBgColor="bg-red-400"
-        textColor="text-red-400"
+        textBgColor="bg-red-300"
+        textColor="text-red-800 dark:text-red-100"
         amount={debt}
         signIcon={
           <Remove className="text-red-600 dark:text-red-600 xs:h-4 xs:w-2 md:h-5 md:w-3" />
