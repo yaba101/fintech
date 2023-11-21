c81ca94bcf0ba99319d5edd3b2f4ced5b44a1bdc
Yeabsira Mekuria <yabumek46@gmail.com>
2023-11-21 23:43:21 +0300

    remove redirect

--- next.config.js
+++ next.config.js
@@ -10,15 +10,7 @@ const nextConfig = {
             },
         ],
     },
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/dashboard',
-                permanent: true,
-            },
-        ]
-    },
+
 }
 
 module.exports = nextConfig
